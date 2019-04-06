aea024654d26b0f22432c16aedcb230e7c246199
zhjiehua <501731591@qq.com>
2019-04-06 15:57:19 +0800

    官方例程

---
+++ index.js
@@ -0,0 +1,27 @@
+const aliyunIot = require('aliyun-iot-device-sdk');
+const deviceConfig = require('./device_id_password.json');
+
+const device = aliyunIot.device(deviceConfig);
+
+device.on('connect', () => {
+  console.log('Connect successfully!');
+  console.log('Post properties every 5 seconds...');
+  setInterval(() => {
+    const params = {
+      Status: 1,
+      Data: 'Hello, world!',
+      Temperature: 25.3,
+      Humidity: 80.2
+    };
+    //console.log(`Post properties: ${JSON.stringify(params)}`);
+    device.postProps(params);
+  }, 5000);
+  
+  device.serve('property/set', (data) => {
+    console.log('Received a message: ', JSON.stringify(data));
+  });
+});
+
+device.on('error', err => {
+  console.error(err);
+});
